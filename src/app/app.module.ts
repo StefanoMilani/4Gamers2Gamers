395a3bdf8f0fc20a89ce398dba78da481c41e851
MilaniStef <stefano.milani96@gmail.com>
2019-05-15 12:26:51 +0200

    add user db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Http client
+import { HttpClientModule } from '@angular/common/http';
+// In memory data service
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService, { dataEncapsulation: false })
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,33 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { User } from './user';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService{
+
+  constructor() { }
+
+  // Overrides the genId method to ensure that a user always has an id.
+  // noinspection JSUnusedGlobalSymbols
+  static genId(users: User[]): number {
+    return users.length > 0 ? Math.max(...users.map(user => user.id)) + 1 : 11;
+  }
+
+  createDb() {
+    const user = [
+      { id: 11, nickname: 'Mr. Nice' },
+      { id: 12, nickname: 'Narco' },
+      { id: 13, nickname: 'Bombasto' },
+      { id: 14, nickname: 'Celeritas' },
+      { id: 15, nickname: 'Magneta' },
+      { id: 16, nickname: 'RubberMan' },
+      { id: 17, nickname: 'Dynama' },
+      { id: 18, nickname: 'Dr IQ' },
+      { id: 19, nickname: 'Magma' },
+      { id: 20, nickname: 'Tornado' }
+    ];
+    return user;
+  }
+}
